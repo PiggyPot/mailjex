0ed6cd392fdcb6c6b6aac33ff88fd5b783c8aee7
Phil McClure <phil.mcclure@gmail.com>
2019-02-04 15:06:48 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule Mailjex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :mailjex,
-     version: "0.1.5",
-     description: "Elixir wrapper for the MailJet API",
-     package: package(),
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :mailjex,
+      version: "0.1.6",
+      description: "Elixir wrapper for the MailJet API",
+      package: package(),
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -17,8 +19,7 @@ defmodule Mailjex.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :httpotion],
-     mod: {Mailjex.Application, []}]
+    [extra_applications: [:logger, :httpotion], mod: {Mailjex.Application, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -31,9 +32,7 @@ defmodule Mailjex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpotion, "~> 3.0.2"},
-     {:poison, "~> 3.0"},
-     {:ex_doc, ">= 0.0.0", only: :dev}]
+    [{:httpotion, "~> 3.0.2"}, {:poison, "~> 3.0"}, {:ex_doc, ">= 0.0.0", only: :dev}]
   end
 
   defp package do
