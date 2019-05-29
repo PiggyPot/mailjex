c2d3bd994e28fc271c8e4f739da7cfdaa924f0b3
j.sayegh <j.sayegh@eweev.com>
2019-05-29 18:55:54 +0300

    Add Worker For Contact List

--- lib/mailjex/api/contact_list.ex
+++ lib/mailjex/api/contact_list.ex
@@ -2,7 +2,7 @@ defmodule Mailjex.Api.ContactList do
   @moduledoc false
   import Mailjex.Utils.Comms
 
-  def manage_contacts_lists(id, body) do
+  def manage_contact(id, body) do
     request(:post, "/REST/contactslist/#{id}/managecontact", body)
     |> decode_json
   end
