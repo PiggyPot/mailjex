c6b2fea0158fc18bcae6af60b2413bf0753f089d
Chris Griffing <cmgriffing@gmail.com>
2019-10-17 09:06:07 -0700

    Add v3 back into urls

--- lib/mailjex/api/contact_list.ex
+++ lib/mailjex/api/contact_list.ex
@@ -3,17 +3,17 @@ defmodule Mailjex.Api.ContactList do
   import Mailjex.Utils.Comms
 
   def manage_contact(id, body) do
-    request(:post, "/REST/contactslist/#{id}/managecontact", body)
+    request(:post, "/v3/REST/contactslist/#{id}/managecontact", body)
     |> decode_json
   end
 
   def manage_contact_in_list(list_id, body) do
-    request(:post, "/REST/contactslist/#{list_id}/managemanycontacts", body)
+    request(:post, "/v3/REST/contactslist/#{list_id}/managemanycontacts", body)
     |> decode_json
   end
 
   def create_contact_list(name, is_deleted = false) do
-    request(:post, "/REST/contactslist", %{
+    request(:post, "/v3/REST/contactslist", %{
       Name: name,
       IsDeleted: is_deleted
     })
@@ -21,22 +21,22 @@ defmodule Mailjex.Api.ContactList do
   end
 
   def get_all_contact_lists(options \\ []) do
-    request(:get, "/REST/contactslist", options)
+    request(:get, "/v3/REST/contactslist", options)
     |> decode_json
   end
 
   def get_contact_list(list_id, options \\ []) do
-    request(:get, "/REST/contactslist/#{list_id}", options)
+    request(:get, "/v3/REST/contactslist/#{list_id}", options)
     |> decode_json
   end
 
   def update_contact_list(list_id, body) do
-    request(:put, "/REST/contactslist/#{list_id}", body)
+    request(:put, "/v3/REST/contactslist/#{list_id}", body)
     |> decode_json
   end
 
   def delete_contact_list(list_id, options \\ []) do
-    request(:delete, "/REST/contactslist/#{list_id}", options)
+    request(:delete, "/v3/REST/contactslist/#{list_id}", options)
     |> decode_json
   end
 
